87b128f754aa7bd3ea7c1fa4db42ddc95d9cb20e
Alfyando <alfyando2001@gmail.com>
2022-12-03 07:06:23 +0700

    add img profile user

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -47,6 +47,7 @@ export const Profile = () => {
       ) : (
         <div className="mx-5 sm:mx-10 md:mx-20 lg:mx-40 my-5">
           <h1>My Profile</h1>
+          <img src="https://ui-avatars.com/api/?name=alfyando&background=random" alt="user" className="rounded-full" />
           <h1>Name: {user.name}</h1>
           <h1>Email: {user.email}</h1>
           <h1>Username: {user.username}</h1>
